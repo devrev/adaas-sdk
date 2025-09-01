359e9cf1e721947598138ed3979ce4e93e185480
Gašper Zgonec <gasper.zgonec@devrev.ai>
2025-09-01 10:48:19 +0200

    Removed test splitting. Now a single test is available, which runs all tests sequentially.

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,7 +2,5 @@ module.exports = {
   preset: 'ts-jest',
   testPathIgnorePatterns: [
     './dist/',
-    // Exclude timeout tests by default - they should only run with test:full or test:cov
-    './src/tests/timeout-handling/'
   ],
 };
