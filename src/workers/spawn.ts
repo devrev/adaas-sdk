0fa7161920dc19fb2cf7c8116389fab5c4347768
rado <radovanjorgic14@gmail.com>
2025-11-04 17:41:17 +0100

    Use console instead of logger|

--- src/workers/spawn.ts
+++ src/workers/spawn.ts
@@ -93,14 +93,15 @@ export async function spawn<ConnectorState>({
   initialDomainMapping,
   options,
 }: SpawnFactoryInterface<ConnectorState>): Promise<void> {
-  const logger = new Logger({ event, options });
+  // eslint-disable-next-line no-global-assign
+  console = new Logger({ event, options });
   const script = getWorkerPath({
     event,
     connectorWorkerPath: workerPath,
   });
 
   if (options?.isLocalDevelopment) {
-    logger.warn(
+    console.warn(
       'WARN: isLocalDevelopment is deprecated. Please use the -- local flag instead.'
     );
   }
@@ -133,11 +134,11 @@ export async function spawn<ConnectorState>({
         });
       });
     } catch (error) {
-      logger.error('Worker error while processing task', error);
+      console.error('Worker error while processing task', error);
       return Promise.reject(error);
     }
   } else {
-    logger.error(
+    console.error(
       'Script was not found for event type: ' + event.payload.event_type + '.'
     );
 
@@ -155,7 +156,7 @@ export async function spawn<ConnectorState>({
         },
       });
     } catch (error) {
-      logger.error('Error while emitting event.', serializeError(error));
+      console.error('Error while emitting event.', serializeError(error));
       return Promise.reject(error);
     }
   }
@@ -169,25 +170,23 @@ export class Spawn {
   private softTimeoutTimer: ReturnType<typeof setTimeout> | undefined;
   private hardTimeoutTimer: ReturnType<typeof setTimeout> | undefined;
   private memoryMonitoringInterval: ReturnType<typeof setInterval> | undefined;
-  private logger: Logger;
   private resolve: (value: void | PromiseLike<void>) => void;
+  private logger: Logger;
 
   constructor({ event, worker, options, resolve }: SpawnInterface) {
     this.alreadyEmitted = false;
     this.event = event;
-    this.logger = new Logger({ event, options });
     this.lambdaTimeout = options?.timeout
       ? Math.min(options.timeout, this.defaultLambdaTimeout)
       : this.defaultLambdaTimeout;
     this.resolve = resolve;
-
-    console.log('Event in main thread', event);
+    this.logger = new Logger({ event, options });
 
     // If soft timeout is reached, send a message to the worker to gracefully exit.
     this.softTimeoutTimer = setTimeout(
       () =>
         void (async () => {
-          this.logger.log(
+          console.log(
             'SOFT TIMEOUT: Sending a message to the worker to gracefully exit.'
           );
           if (worker) {
@@ -206,7 +205,7 @@ export class Spawn {
     this.hardTimeoutTimer = setTimeout(
       () =>
         void (async () => {
-          this.logger.error(
+          console.error(
             'HARD TIMEOUT: Worker did not exit in time. Terminating the worker.'
           );
           if (worker) {
@@ -225,7 +224,7 @@ export class Spawn {
       WorkerEvent.WorkerExit,
       (code: number) =>
         void (async () => {
-          this.logger.info('Worker exited with exit code: ' + code + '.');
+          console.info('Worker exited with exit code: ' + code + '.');
           this.clearTimeouts();
           await this.exitFromMainThread();
         })()
@@ -242,7 +241,7 @@ export class Spawn {
 
       // If worker sends a message that it has emitted an event, then set alreadyEmitted to true.
       if (message?.subject === WorkerMessageSubject.WorkerMessageEmitted) {
-        this.logger.info('Worker has emitted message to ADaaS.');
+        console.info('Worker has emitted message to ADaaS.');
         this.alreadyEmitted = true;
       }
     });
@@ -252,11 +251,11 @@ export class Spawn {
       try {
         const memoryInfo = getMemoryUsage();
         if (memoryInfo) {
-          this.logger.info(memoryInfo.formattedMessage);
+          console.info(memoryInfo.formattedMessage);
         }
       } catch (error) {
         // If memory monitoring fails, log the warning and clear the interval to prevent further issues
-        this.logger.warn(
+        console.warn(
           'Memory monitoring failed, stopping logging of memory usage interval',
           error
         );
