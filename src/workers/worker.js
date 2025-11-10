12abfeb47b777fc1b22d237d6121a835634fecc1
rado <radovanjorgic14@gmail.com>
2025-11-10 22:39:46 +0100

    Fix comment in worker.js

--- src/workers/worker.js
+++ src/workers/worker.js
@@ -2,11 +2,8 @@ const { workerData } = require('worker_threads');
 
 require('ts-node').register();
 
-// Override console with Logger instance
 const { Logger } = require('../logger/logger');
-
 // eslint-disable-next-line no-global-assign
 console = new Logger({ event: workerData.event, options: workerData.options });
 
-// Now load the actual worker with console already overridden
 require(workerData.workerPath);
