6df0157cdfe437cd291c7f7af7ce8dcccfeec606
Gašper Zgonec <gasper.zgonec@devrev.ai>
2025-11-13 11:28:20 +0100

    Removed redundant default workers and added default paths for workers inside of spawn.

--- src/types/workers.ts
+++ src/types/workers.ts
@@ -3,7 +3,7 @@ import { Worker } from 'worker_threads';
 import { State } from '../state/state';
 import { WorkerAdapter } from '../workers/worker-adapter';
 
-import { AirdropEvent, ExtractorEventType } from './extraction';
+import { AirdropEvent, EventType, ExtractorEventType } from './extraction';
 
 import { LoaderEventType } from './loading';
 
@@ -30,11 +30,13 @@ export interface WorkerAdapterInterface<ConnectorState> {
  * @param {boolean=} isLocalDevelopment - A flag to indicate if the adapter is being used in local development
  * @param {number=} timeout - The timeout for the worker thread
  * @param {number=} batchSize - Maximum number of extracted items in a batch
+ * @param {Partial<Record<EventType, string>>=} worker_path_overrides - A map of event types to custom worker paths to override default worker paths
  */
 export interface WorkerAdapterOptions {
   isLocalDevelopment?: boolean;
   timeout?: number;
   batchSize?: number;
+  worker_path_overrides?: Partial<Record<EventType, string>>;
 }
 
 /**
