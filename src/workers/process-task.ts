a86ff66a3019734b982e51a9ea3949f85cb27a2e
Gašper Zgonec <gasper.zgonec@devrev.ai>
2025-11-18 15:37:10 +0100

    Separated out the calls to make direct 'sdk_log: true' calls for logs that should output it.

--- src/workers/process-task.ts
+++ src/workers/process-task.ts
@@ -1,10 +1,7 @@
 import { isMainThread, parentPort, workerData } from 'node:worker_threads';
 import { translateIncomingEventType } from '../common/event-type-translation';
 import { Logger, serializeError } from '../logger/logger';
-import {
-  runWithSdkLogContext,
-  runWithUserLogContext,
-} from '../logger/logger.context';
+import { runWithUserLogContext } from '../logger/logger.context';
 import { createAdapterState } from '../state/state';
 import {
   ProcessTaskInterface,
@@ -17,66 +14,64 @@ export function processTask<ConnectorState>({
   task,
   onTimeout,
 }: ProcessTaskInterface<ConnectorState>) {
-  return runWithSdkLogContext(() => {
-    if (!isMainThread) {
-      void (async () => {
-        try {
-          const event = workerData.event;
+  if (!isMainThread) {
+    void (async () => {
+      try {
+        const event = workerData.event;
 
-          // TODO: Remove when the old types are completely phased out
-          event.payload.event_type = translateIncomingEventType(
-            event.payload.event_type
-          );
+        // TODO: Remove when the old types are completely phased out
+        event.payload.event_type = translateIncomingEventType(
+          event.payload.event_type
+        );
 
-          const initialState = workerData.initialState as ConnectorState;
-          const initialDomainMapping = workerData.initialDomainMapping;
-          const options = workerData.options;
-          // eslint-disable-next-line no-global-assign
-          console = new Logger({ event, options });
+        const initialState = workerData.initialState as ConnectorState;
+        const initialDomainMapping = workerData.initialDomainMapping;
+        const options = workerData.options;
+        // eslint-disable-next-line no-global-assign
+        console = new Logger({ event, options });
 
-          const adapterState = await createAdapterState<ConnectorState>({
+        const adapterState = await createAdapterState<ConnectorState>({
+          event,
+          initialState,
+          initialDomainMapping,
+          options,
+        });
+
+        if (parentPort && workerData.event) {
+          const adapter = new WorkerAdapter<ConnectorState>({
             event,
-            initialState,
-            initialDomainMapping,
+            adapterState,
             options,
           });
 
-          if (parentPort && workerData.event) {
-            const adapter = new WorkerAdapter<ConnectorState>({
-              event,
-              adapterState,
-              options,
-            });
+          parentPort.on(
+            WorkerEvent.WorkerMessage,
+            (message) =>
+              void (async () => {
+                if (
+                  message.subject === WorkerMessageSubject.WorkerMessageExit
+                ) {
+                  console.log(
+                    'Worker received message to gracefully exit. Setting isTimeout flag and executing onTimeout function.'
+                  );
 
-            parentPort.on(
-              WorkerEvent.WorkerMessage,
-              (message) =>
-                void (async () => {
-                  if (
-                    message.subject === WorkerMessageSubject.WorkerMessageExit
-                  ) {
-                    console.log(
-                      'Worker received message to gracefully exit. Setting isTimeout flag and executing onTimeout function.'
-                    );
+                  adapter.handleTimeout();
+                  await runWithUserLogContext(() => onTimeout({ adapter }));
 
-                    adapter.handleTimeout();
-                    await runWithUserLogContext(() => onTimeout({ adapter }));
-
-                    console.log(
-                      'Finished executing onTimeout function. Exiting worker.'
-                    );
-                    process.exit(0);
-                  }
-                })()
-            );
-            await runWithUserLogContext(() => task({ adapter }));
-            process.exit(0);
-          }
-        } catch (error) {
-          console.error('Error while processing task.', serializeError(error));
-          process.exit(1);
+                  console.log(
+                    'Finished executing onTimeout function. Exiting worker.'
+                  );
+                  process.exit(0);
+                }
+              })()
+          );
+          await runWithUserLogContext(() => task({ adapter }));
+          process.exit(0);
         }
-      })();
-    }
-  });
+      } catch (error) {
+        console.error('Error while processing task.', serializeError(error));
+        process.exit(1);
+      }
+    })();
+  }
 }
