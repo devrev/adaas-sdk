63f013ca3061279913d930d45ff2c16f27a35b7e
Gašper Zgonec <gasper.zgonec@devrev.ai>
2025-11-18 14:05:09 +0100

    Added context detection for sdk_log flag.

--- src/workers/process-task.ts
+++ src/workers/process-task.ts
@@ -1,5 +1,9 @@
 import { isMainThread, parentPort, workerData } from 'node:worker_threads';
 import { Logger, serializeError } from '../logger/logger';
+import {
+  runWithSdkLogContext,
+  runWithUserLogContext,
+} from '../logger/logger.context';
 import { createAdapterState } from '../state/state';
 import {
   ProcessTaskInterface,
@@ -12,58 +16,60 @@ export function processTask<ConnectorState>({
   task,
   onTimeout,
 }: ProcessTaskInterface<ConnectorState>) {
-  if (!isMainThread) {
-    void (async () => {
-      try {
-        const event = workerData.event;
-        const initialState = workerData.initialState as ConnectorState;
-        const initialDomainMapping = workerData.initialDomainMapping;
-        const options = workerData.options;
-        // eslint-disable-next-line no-global-assign
-        console = new Logger({ event, options });
+  return runWithSdkLogContext(() => {
+    if (!isMainThread) {
+      void (async () => {
+        try {
+          const event = workerData.event;
+          const initialState = workerData.initialState as ConnectorState;
+          const initialDomainMapping = workerData.initialDomainMapping;
+          const options = workerData.options;
+          // eslint-disable-next-line no-global-assign
+          console = new Logger({ event, options });
 
-        const adapterState = await createAdapterState<ConnectorState>({
-          event,
-          initialState,
-          initialDomainMapping,
-          options,
-        });
-
-        if (parentPort && workerData.event) {
-          const adapter = new WorkerAdapter<ConnectorState>({
+          const adapterState = await createAdapterState<ConnectorState>({
             event,
-            adapterState,
+            initialState,
+            initialDomainMapping,
             options,
           });
 
-          parentPort.on(
-            WorkerEvent.WorkerMessage,
-            (message) =>
-              void (async () => {
-                if (
-                  message.subject === WorkerMessageSubject.WorkerMessageExit
-                ) {
-                  console.log(
-                    'Worker received message to gracefully exit. Setting isTimeout flag and executing onTimeout function.'
-                  );
+          if (parentPort && workerData.event) {
+            const adapter = new WorkerAdapter<ConnectorState>({
+              event,
+              adapterState,
+              options,
+            });
 
-                  adapter.handleTimeout();
-                  await onTimeout({ adapter });
+            parentPort.on(
+              WorkerEvent.WorkerMessage,
+              (message) =>
+                void (async () => {
+                  if (
+                    message.subject === WorkerMessageSubject.WorkerMessageExit
+                  ) {
+                    console.log(
+                      'Worker received message to gracefully exit. Setting isTimeout flag and executing onTimeout function.'
+                    );
 
-                  console.log(
-                    'Finished executing onTimeout function. Exiting worker.'
-                  );
-                  process.exit(0);
-                }
-              })()
-          );
-          await task({ adapter });
-          process.exit(0);
+                    adapter.handleTimeout();
+                    await runWithUserLogContext(() => onTimeout({ adapter }));
+
+                    console.log(
+                      'Finished executing onTimeout function. Exiting worker.'
+                    );
+                    process.exit(0);
+                  }
+                })()
+            );
+            await runWithUserLogContext(() => task({ adapter }));
+            process.exit(0);
+          }
+        } catch (error) {
+          console.error('Error while processing task.', serializeError(error));
+          process.exit(1);
         }
-      } catch (error) {
-        console.error('Error while processing task.', serializeError(error));
-        process.exit(1);
-      }
-    })();
-  }
+      })();
+    }
+  });
 }
