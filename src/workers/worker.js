e93c76486ce18be00e278c27f8b4615aa8eda1cb
Gašper Zgonec <gasper.zgonec@devrev.ai>
2025-11-18 17:31:18 +0100

    Reverted the deprecated files and fixed tests.

--- src/workers/worker.js
+++ src/workers/worker.js
@@ -5,7 +5,7 @@ require('ts-node').register();
 const { Logger } = require('../logger/logger');
 const { runWithUserLogContext } = require('../logger/logger.context');
 
-// eslint-disable-next-line no-global-assign
+ 
 console = new Logger({ event: workerData.event, options: workerData.options });
 
 runWithUserLogContext(() => {
