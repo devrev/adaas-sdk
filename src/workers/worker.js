7e218d3748eb019a13e16da03287b0e2a24b2ef9
Gašper Zgonec <gasper.zgonec@devrev.ai>
2025-12-02 08:14:30 +0100

    Updated enums, fixed nits and removed unused mocks and functions.

--- src/workers/worker.js
+++ src/workers/worker.js
@@ -1,11 +1,10 @@
-const { workerData } = require('worker_threads');
+const { workerData } = require('node:worker_threads');
 
 require('ts-node').register();
 
 const { Logger } = require('../logger/logger');
 const { runWithUserLogContext } = require('../logger/logger.context');
 
- 
 console = new Logger({ event: workerData.event, options: workerData.options });
 
 runWithUserLogContext(() => {
